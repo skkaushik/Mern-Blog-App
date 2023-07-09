9cfe94f08ae851b4e9b9045ec529f425a3932e39
sachin <sksachinkaushik5362@gmail.com>
2023-07-09 22:13:05 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import {Routes,Route} from 'react-router-dom'
-import Blogs from './pages/Blogs';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Blogs from "./pages/Blogs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      
+    <>
+      <Header />
       <Routes>
-      <Header/>
-<Route path='/' element={<Blogs/>}/>
-<Route path='/login' element={<Login/>}/>
-<Route path='/register' element={<Register/>}/>
+        <Route path="/" element={<Blogs />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-      
-    </div>
+    </>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,32 @@
-import React from "react";
-import { Box, Toolbar, AppBar, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Toolbar,
+  AppBar,
+  Button,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+
 function Header() {
+  const [value, setValue] = useState();
   return (
     <>
       <AppBar position="sticky">
         <Toolbar>
           <Typography variant="h4">My Blog App</Typography>
+          <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+              <Tab label=" My Blogs" LinkComponent={Link} to="/my-blogs" />
+            </Tabs>
+          </Box>
           <Box display={"flex"} marginLeft={"auto"}>
             <Button
               sx={{ margin: 1, color: "white" }}
