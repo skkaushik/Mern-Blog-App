bc59d265abf28cfcd946d748e946a3e1baf14d0a
sachin <sksachinkaushik5362@gmail.com>
2023-07-09 18:07:22 +0530

    commit

--- backend/controllers/blogControlller.js
+++ backend/controllers/blogControlller.js
@@ -31,15 +31,15 @@ exports.getAllBlogsController = async (req, res) => {
 
 //Create Blog
 exports.createBlogController = async (req, res) => {
-//   try {
-//     const { title, description, image, user } = req.body;
-//     //validation
-//     if (!title || !description || !image || !user) {
-//       return res.status(400).send({
-//         success: false,
-//         message: "Please Provide ALl Fields",
-//       });
-//     }
+  try {
+    const { title, description, image} = req.body;
+    //validation
+    if (!title || !description || !image) {
+      return res.status(400).send({
+        success: false,
+        message: "Please Provide ALl Fields",
+      });
+    }
 //     const exisitingUser = await userModel.findById(user);
 //     //validaton
 //     if (!exisitingUser) {
@@ -49,27 +49,27 @@ exports.createBlogController = async (req, res) => {
 //       });
 //     }
 
-//     const newBlog = new blogModel({ title, description, image, user });
+    const newBlog = new blogModel({ title, description, image});
 //     const session = await mongoose.startSession();
 //     session.startTransaction();
 //     await newBlog.save({ session });
 //     exisitingUser.blogs.push(newBlog);
 //     await exisitingUser.save({ session });
 //     await session.commitTransaction();
-//     await newBlog.save();
-//     return res.status(201).send({
-//       success: true,
-//       message: "Blog Created!",
-//       newBlog,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(400).send({
-//       success: false,
-//       message: "Error WHile Creting blog",
-//       error,
-//     });
-//   }
+    await newBlog.save();
+    return res.status(201).send({
+      success: true,
+      message: "Blog Created!",
+      newBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+      message: "Error WHile Creting blog",
+      error,
+    });
+  }
 };
 
 //Update Blog
