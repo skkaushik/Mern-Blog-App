048a1dd1a2ceb6c46699cb167905a523781bd50d
sachin <sksachinkaushik5362@gmail.com>
2023-07-09 19:06:58 +0530

    commit

--- backend/controllers/blogControlller.js
+++ backend/controllers/blogControlller.js
@@ -1,6 +1,8 @@
-const mongoose = require("mongoose");
+
+const mongoose  = require("mongoose");
 const blogModel = require("../models/blogModel");
 const userModel = require("../models/userModel");
+
 
 //GET ALL BLOGS
 exports.getAllBlogsController = async (req, res) => {
@@ -32,30 +34,30 @@ exports.getAllBlogsController = async (req, res) => {
 //Create Blog
 exports.createBlogController = async (req, res) => {
   try {
-    const { title, description, image } = req.body;
+    const { title, description, image, user } = req.body;
     //validation
-    if (!title || !description || !image) {
+    if (!title || !description || !image || !user) {
       return res.status(400).send({
         success: false,
         message: "Please Provide ALl Fields",
       });
     }
-    //     const exisitingUser = await userModel.findById(user);
-    //     //validaton
-    //     if (!exisitingUser) {
-    //       return res.status(404).send({
-    //         success: false,
-    //         message: "unable to find user",
-    //       });
-    //     }
+        const exisitingUser = await userModel.findById(user);
+        //validaton
+        if (!exisitingUser) {
+          return res.status(404).send({
+            success: false,
+            message: "unable to find user",
+          });
+        }
 
-    const newBlog = new blogModel({ title, description, image });
-    //     const session = await mongoose.startSession();
-    //     session.startTransaction();
-    //     await newBlog.save({ session });
-    //     exisitingUser.blogs.push(newBlog);
-    //     await exisitingUser.save({ session });
-    //     await session.commitTransaction();
+    const newBlog = new blogModel({ title, description, image,user});
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await newBlog.save({ session });
+        exisitingUser.blogs.push(newBlog);
+        await exisitingUser.save({ session });
+        await session.commitTransaction( );
     await newBlog.save();
     return res.status(201).send({
       success: true,
