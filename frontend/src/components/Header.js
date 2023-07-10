6384a6c1a021b8b0bfb5c814096965a152bc3c25
sachin <sksachinkaushik5362@gmail.com>
2023-07-10 16:33:39 +0530

    commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,17 +10,14 @@ import {
   Tab,
 } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useSelector ,useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { authActions } from "../redux/store";
-import toast  from "react-hot-toast";
-
-
-
+import toast from "react-hot-toast";
 
 function Header() {
-    let isLogin=useSelector(state=>state.isLogin)
-    // console.log(isLogin);
-    isLogin = isLogin || localStorage.getItem("userId");
+  let isLogin = useSelector((state) => state.isLogin);
+  // console.log(isLogin);
+  isLogin = isLogin || localStorage.getItem("userId");
   const [value, setValue] = useState();
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -40,43 +37,49 @@ function Header() {
     <>
       <AppBar position="sticky">
         <Toolbar>
-          <Typography variant="h4">My Blog App</Typography>
+          <Typography variant="h4">Blogopedia</Typography>
           {isLogin && (
             <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
-            <Tabs
-              textColor="inherit"
-              value={value}
-              onChange={(e, val) => setValue(val)}
-            >
-              <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
-              <Tab label=" My Blogs" LinkComponent={Link} to="/my-blogs" />
-              <Tab label=" Create Blog" LinkComponent={Link} to="/create-blog" />
-            </Tabs>
-          </Box>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label=" My Blogs" LinkComponent={Link} to="/my-blogs" />
+                <Tab
+                  label=" Create Blog"
+                  LinkComponent={Link}
+                  to="/create-blog"
+                />
+              </Tabs>
+            </Box>
           )}
-          
+
           <Box display={"flex"} marginLeft={"auto"}>
-           {!isLogin && (
-            <>
-             <Button
-              sx={{ margin: 1, color: "white" }}
-              LinkComponent={Link}
-              to="/login"
-            >
-              Login
-            </Button>
-            <Button
-              sx={{ margin: 1, color: "white" }}
-              LinkComponent={Link}
-              to="/register"
-            >
-              Register
-            </Button>
-            </>
-           )}
-           {isLogin &&(
-             <Button onClick={handleLogout}  sx={{ margin: 1, color: "white" }}>Logout</Button>
-           )}
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
+            {isLogin && (
+              <Button onClick={handleLogout} sx={{ margin: 1, color: "white" }}>
+                Logout
+              </Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
